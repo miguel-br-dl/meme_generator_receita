09ce91c97e80a8ce8ad9dcc95971c61e6a11827b
Miguel Angelo <miguel.a.moutinho.neto@gmail.com>
2026-02-14 10:47:15 -0300

    ajustes para  amarelo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ToolbarModule } from 'primeng/toolbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ToolbarModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
